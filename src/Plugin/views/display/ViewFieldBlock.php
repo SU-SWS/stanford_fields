15d3541a177c33045edf2e7783f9408bea13ecaa
pookmish <pookmish@stanford.edu>
2021-03-02 08:08:58 -0800

    D8CORE-3476 Create a new view display mode specific for viewfields (#14)

---
+++ src/Plugin/views/display/ViewFieldBlock.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\stanford_fields\Plugin\views\display;
+
+use Drupal\views\Plugin\views\display\Block;
+
+/**
+ * The plugin that handles a block.
+ *
+ * @ingroup views_display_plugins
+ *
+ * @ViewsDisplay(
+ *   id = "viewfield_block",
+ *   title = @Translation("View Field Block"),
+ *   help = @Translation("Identical to a block, but allows for granular viewfield settings."),
+ *   theme = "views_view",
+ *   register_theme = FALSE,
+ *   uses_hook_block = TRUE,
+ *   contextual_links_locations = {"block"},
+ *   admin = @Translation("Block")
+ * )
+ *
+ * @see \Drupal\views\Plugin\Block\ViewsBlock
+ * @see \Drupal\views\Plugin\Derivative\ViewsBlock
+ */
+class ViewFieldBlock extends Block {
+
+}
