fd4a253bd3be592b1c7fa014d8d4185334d2a14a
Ian Monroe <ian@ianmonroe.com>
2021-11-17 12:31:58 -0700

    Working kernel test

--- tests/src/Kernel/Plugin/Field/FieldWidget/LocalistUrlWidgetTest.php
+++ tests/src/Kernel/Plugin/Field/FieldWidget/LocalistUrlWidgetTest.php
@@ -3,17 +3,15 @@
 namespace Drupal\Tests\stanford_fields\Kernel\Plugin\Field\FieldWidget;
 
 use Drupal\Core\Entity\Entity\EntityFormDisplay;
-use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Form\FormState;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\node\Entity\Node;
 use Drupal\node\Entity\NodeType;
-use Drupal\stanford_fields\Plugin\Field\FieldWidget\LocalistUrlWidget;
 
 /**
- * Class LocalistUrlWidgetTest
+ * Class LocalistUrlWidgetTest.
  *
  * @group
  * @coversDefaultClass \Drupal\stanford_fields\Plugin\Field\FieldWidget\LocalistUrlWidget
@@ -42,7 +40,6 @@ class LocalistUrlWidgetTest extends KernelTestBase {
     $this->installEntitySchema('node');
     $this->installEntitySchema('field_config');
     $this->installConfig(['system', 'field', 'link']);
-    //$this->installEntitySchema('date_format');
 
     NodeType::create(['type' => 'page'])->save();
 
@@ -60,15 +57,6 @@ class LocalistUrlWidgetTest extends KernelTestBase {
       'bundle' => 'page',
     ]);
     $field->save();
-
-    // Set the cache so we don't have to fetch xml as we have other tests
-    // for that.
-    $key_val = [
-      'one' => 'One',
-      'two' => 'Two',
-      'three' => 'Three',
-    ];
-
 
   }
 
@@ -90,9 +78,9 @@ class LocalistUrlWidgetTest extends KernelTestBase {
     $entity_form_display->setComponent('su_localist_url', [
       'type' => 'localist_url',
       'settings' => [
-        'base_url' => 'https://stanford.enterprise.localist.com'
-        ],
-      ])
+        'base_url' => 'https://stanford.enterprise.localist.com',
+      ],
+    ])
       ->removeComponent('created')
       ->save();
 
@@ -100,7 +88,7 @@ class LocalistUrlWidgetTest extends KernelTestBase {
       [
         'uri' => 'https://stanford.enterprise.localist.com/api/2/events?group_id=37955145294460&days=365',
         'title' => '',
-        'options' => ''
+        'options' => '',
       ],
     ]);
 
@@ -109,34 +97,26 @@ class LocalistUrlWidgetTest extends KernelTestBase {
 
     $entity_form_display->buildForm($node, $form, $form_state);
 
-    $widget_value = $form['field_date']['widget'][0]['value'];
+    $widget_value = $form['su_localist_url']['widget'];
 
-    $this->assertEquals('link', $widget_value['#type']);
-    $this->assertEquals('uri', $widget_value['#uri']);
-  }
+    $this->assertIsArray($widget_value[0]);
+    $this->assertEquals('https://stanford.enterprise.localist.com/api/2/events?group_id=37955145294460&days=365', $widget_value[0]['uri']['#default_value']);
+    $this->assertEquals('details', $widget_value[0]['filters']['#type']);
+    $this->assertIsArray($widget_value[0]['filters']['type']['event_audience']);
+    $this->assertIsArray($widget_value[0]['filters']['type']['event_audience']['#options']);
+    $this->assertContains('Students', $widget_value[0]['filters']['type']['event_audience']['#options']);
+    $this->assertContains('Everyone', $widget_value[0]['filters']['type']['event_audience']['#options']);
+    $this->assertIsArray($widget_value[0]['filters']['type']['event_subject']['#options']);
+    $this->assertContains('Arts/Media', $widget_value[0]['filters']['type']['event_subject']['#options']);
+    $this->assertIsArray($widget_value[0]['filters']['type']['event_types']['#options']);
+    $this->assertContains('Class/Seminar', $widget_value[0]['filters']['type']['event_types']['#options']);
+    $this->assertIsArray($widget_value[0]['filters']['group_id']);
+    $this->assertContains('Stanford Web Services', $widget_value[0]['filters']['group_id']['#options']);
+    $this->assertFalse($widget_value[0]['filters']['group_id']['#multiple']);
+    $this->assertIsArray($widget_value[0]['filters']['venue_id']);
+    $this->assertFalse($widget_value[0]['filters']['venue_id']['#multiple']);
+    $this->assertContains('Cardinal Hall', $widget_value[0]['filters']['venue_id']['#options']);
 
-  /**
-   * Test the settings form and the summary.
-   */
-  public function testSettingsForm() {
-    $ten_years = 60 * 60 * 24 * 365 * 10;
-
-    $field_def = $this->createMock(FieldDefinitionInterface::class);
-    $config = [
-      'field_definition' => $field_def,
-      'settings' => [],
-      'third_party_settings' => [],
-    ];
-    $definition = [];
-    $widget = LocalistUrlWidget::create(\Drupal::getContainer(), $config, '', $definition);
-    $summary = [];
-    $this->assertEquals($summary, $widget->settingsSummary()[0]);
-
-    $form = [];
-    $form_state = new FormState();
-    $element = $widget->settingsForm($form, $form_state);
-
-    $this->assertEquals($element, $element['start']['#default_value']);
   }
 
 }
